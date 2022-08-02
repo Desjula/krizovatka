25fe2185f742b5dd3aa5dd503862c12d8875f947
Julie Desová <juliedesova@seznam.cz>
2022-08-02 15:39:41 +0200

    zakladni navrh

---
+++ script.js
@@ -0,0 +1,8 @@
+/*let ZelenaCervena = document.querySelector("blikani--r");
+ZelenaCervena.style.color = "green";*/
+
+
+function Probliknuti(){
+    let ZelenaCervena = document.querySelector(".blikani--r");
+    ZelenaCervena.className("blikani--g");
+}
