cc9f3f8f2cb943cb6254656c03404d60225e2789
Julie Desová <juliedesova@seznam.cz>
2022-08-03 14:08:13 +0200

    semafor rozpracovanz kod

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ let client = new Paho.MQTT.Client(
     'letni-skola' + Math.random()
 )
 
+let svetlo = document.querySelector(".svetlo");
 
+let barvy = ["red", "orange", "green"]
+let pocetTiku = 0
+let semaforAuta
+
+let stop = document.querySelector(".stop");
+stop.addEventListener("click", function(){
+    clearInterval(semaforAuta);
+})
+
+
+let start = document.querySelector(".start");
+start.addEventListener("click", function(){
+    semaforAuta = setInterval(function(){
+    svetlo.style.backgroundColor = barvy[pocetTiku]
+
+if (pocetTiku > barvy.length){
+    pocetTiku = 0}
+},2000);
+})
