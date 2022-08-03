cfb968417db456f0b481abff5e99fd261573f4c2
Julie Desová <juliedesova@seznam.cz>
2022-08-03 16:14:03 +0200

    spojeno s MQTT

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-// let client = new Paho.MQTT.Client(
-//     'd57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud',
-//     8884,
-//     'letni-skola' + Math.random()
-// )
+let client = new Paho.MQTT.Client(
+    'd57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud',
+    8884,
+    'letni-skola' + Math.random()
+)
+
+function onConnect() {
+    console.log("onConnect");
+    client.onMessageArrived = onMessageArrived;
+    client.subscribe("/crossing/#");
+    client.subscribe("/rail-crossing/#");
+}
+
+function onMessageArrived(message) {
+
+    console.log(message.destinationName);
+    console.log(message.payloadString);
+}
+
+client.connect({
+    onSuccess: onConnect,
+    userName: "robot",
+    password: "P@ssW0rd!",
+    useSSL: true
+});
+
 
 let svetlo = document.querySelector(".svetlo");
 let pocetTiku = 0
@@ -18,7 +39,7 @@ stop.addEventListener("click", function(){
 
 let KZelene = document.querySelector(".KZelene");
 KZelene.addEventListener("click", function(){
-    zmenSemafor("zelena",svetlo)
+    zmenSemafor("zelena",svetlo);
 })
 
 let KCervene = document.querySelector(".KCervene");
@@ -29,9 +50,9 @@ KCervene.addEventListener("click", function(){
 function zmenSemafor(kCemu,cisloSemaforu) {
     let barvy = [] 
     if (kCemu == "zelena"){
-        barvy =  ["red", "orange", "green"]
+        barvy =  ["#FF0000", "#FFA500", "#00FF00"]
     }else {
-        barvy = ["green", "orange", "red"]
+        barvy = ["#00FF00", "#FFA500", "#FF0000"]
     }
     semafor = setInterval(function(){
         urcitSemafor(cisloSemaforu)
@@ -39,10 +60,12 @@ function zmenSemafor(kCemu,cisloSemaforu) {
         clearInterval(semafor)
         pocetTiku = 0 
     }
+    
     },1000)
     function urcitSemafor(cisloSemaforu){
         cisloSemaforu.style.backgroundColor = barvy[pocetTiku]
         pocetTiku += 1
     }
-
 }
+
+zmenSemafor("zelena", svetlo)
