a87b6cce1c7860315e519fa79b0193f4456ce68d
Anna Dolejší <dolejsianna@email.cz>
2022-08-05 10:02:31 +0200

    uplne vyresen system semaforu

--- logika.js
+++ logika.js
@@ -16,7 +16,7 @@ let prechodLevyNahore
 let start = document.querySelector('.start')
 start = addEventListener('click', function() {
     puvodniStav()
-    this.setInterval(spustCyklus(), 1000)
+    this.setInterval(spustCyklus(), 4000)
 })
 
 let pocitadlo = 0
@@ -44,20 +44,60 @@ function spustCyklus()  {
     }
 }
 function puvodniStav() {
-    //nastav vsechny semafory na cervenou
-}
-function fazePrvni() {
-    
-}
-function fazeDruha() {
+    //nastav vsechny semafory na vychozi stav aby to neblblo pri prvni fazo
+    SemaforKC(semaforJedna)
+    SemaforKC(semaforJednaSipka)
+    SemaforKC(semaforDvaDoleva)
+    SemaforKC(semaforDvaRovne)
+    SemaforKC(semaforTri)
+    SemaforKZ(semaforTriSipka) //
+    SemaforKZ(semaforCtyriDoleva) //
+    SemaforKZ(semaforCtyriRovne) //
+    SemaforKC(prechodLevyDole)
+    SemaforKC(prechodLevyNahore)
+    SemaforKC(prechodPravyDole)
+    SemaforKC(prechodPravyNahore)
 
 }
-function fazeTreti() {
+function fazePrvni() { // Sviti prechody, 4 a 2 - obe rovne. Zhasnou 4 doleva, 3 sipka
+    SemaforKC(semaforCtyriDoleva)
+    SemaforKC(semaforTriSipka)
+
+    SemaforKZ(prechodLevyDole)
+    SemaforKZ(prechodLevyNahore)
+    SemaforKZ(prechodPravyDole)
+    SemaforKZ(prechodPravyNahore)
+    SemaforKZ(semaforDvaRovne)
 
 }
-function fazeCtvrta() {
+function fazeDruha() { // Zhasnou prechody, 4 rovne. Rozsviti 2 doleva, 1 sipka
+    SemaforKC(prechodLevyDole)
+    SemaforKC(prechodLevyNahore)
+    SemaforKC(prechodPravyDole)
+    SemaforKC(prechodPravyNahore)
+    SemaforKC(semaforCtyriRovne)
+
+    SemaforKZ(semaforDvaDoleva)
+    SemaforKZ(semaforJednaSipka)
 
 }
-function fazePata() {
+function fazeTreti() { //Zhasne cela 2, rozsviti se cela 3
+    SemaforKC(semaforDvaDoleva)
+    SemaforKC(semaforDvaRovne)
 
+    SemaforKZ(semaforTri)
+    SemaforKZ(semaforTriSipka)
+
+}
+function fazeCtvrta() { //Zhasne Tri rovne, rozsviti se 1 rovne
+    SemaforKC(semaforTri)
+
+    SemaforKZ(semaforJedna)
+}
+function fazePata() { //Zhasne cela 1, rozsviti se cela 4
+    SemaforKC(semaforJedna)
+    SemaforKC(semaforJednaSipka)
+
+    SemaforKZ(semaforCtyriDoleva)
+    SemaforKZ(semaforCtyriRovne)
 }
